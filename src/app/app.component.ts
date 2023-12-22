36e4d91a264c3750705e095c06a3d1dc4dcc8016
Piotr Ranosz <pranosz@wp.pl>
2023-12-22 16:23:23 +0100

    Signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormArray } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'angular-primeng-todo';
-  /*
-  todos = [
-    {
-      id: 1,
-      task: 'first-task',
-      completed: true
-    },
-    {
-      id: 2,
-      task: 'secound-task',
-      completed: false
-    }
-  ];*/
 
-  todoForm = this.fb.group({
-    tasks: this.fb.array([
-      this.fb.group({ id: 0, task: '', completed: false})
-    ])
-  });
-
-  constructor(private fb: FormBuilder){}
-
-  ngOnInit(): void {
-    console.log("todoForm ", this.todoForm);
-  }
-
-  debugConsole(item: any) {
-    console.log("item ", item);
-  }
-
-  get tasks(): FormArray {
-    return this.todoForm.get('tasks') as FormArray;
-  }
+  private taskService = inject(TaskService);
+  service = this.taskService;
 
   addTask() {
-    const arr = this.todoForm.get('tasks') as FormArray;
-    arr.push(
-      this.fb.group({id: 3, task: '', completed: true})
-    );
-
-    console.log("todoForm 2", this.todoForm);
+    this.taskService.addNewTask();
   }
 
-  onSubmit(): void {
-    console.log("todoForm ", this.todoForm.getRawValue());
+  deleteTask(id: string): void {
+    this.taskService.deleteTask(id);
   }
 
 }
