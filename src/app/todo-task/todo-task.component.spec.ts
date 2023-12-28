c25bea763ceeb9921d1d9f305ea9bcecf20fc902
Piotr Ranosz <pranosz@wp.pl>
2023-12-28 09:01:36 +0100

    tests

--- src/app/todo-task/todo-task.component.spec.ts
+++ src/app/todo-task/todo-task.component.spec.ts
@@ -1,17 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoTaskComponent } from './todo-task.component';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { Task } from '../models/task.interface';
+import { FormsModule } from '@angular/forms';
 
-describe('TodoTaskComponent', () => {
+fdescribe('TodoTaskComponent', () => {
   let component: TodoTaskComponent;
   let fixture: ComponentFixture<TodoTaskComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TodoTaskComponent]
+      declarations: [TodoTaskComponent],
+      imports: [
+        CheckboxModule,
+        ButtonModule,
+        FormsModule
+      ],
+      providers: [
+        ConfirmationService,
+        MessageService
+      ]
     });
+
+    const task: Task = {
+      id: "1",
+      name: "test task",
+      completed: false
+    };
+
     fixture = TestBed.createComponent(TodoTaskComponent);
     component = fixture.componentInstance;
+    component.task = task;
     fixture.detectChanges();
   });
 
